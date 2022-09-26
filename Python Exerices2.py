31fdc9cf59abfe73b7b118709e6ac570475896ee
samir9080 <Samir.israpilov@metropolia.fi>
2022-09-26 14:47:35 +0300

    Exercise2

---
+++ Python Exerices2.py
@@ -0,0 +1,9 @@
+user = input("Enter your name:")
+print("Nice to meet you," + user + "!")
+
+
+import math
+radius = float(input ("Enter the radius of the circle : "))
+
+area = math.pi * radius * radius
+print("Area of the Circle is : {0}".format(area))
